c4bc8af2e1b0cc8bdd49c88f1f03fa90eb2cd2ca
Ramanuj Prasad <ramanuj.prasad@rabobank.nl>
2021-03-14 20:32:44 +0100

    feat(post-app): added spinner module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,20 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostDetailsComponent } from './components/post-details/post-details.component';
 import { PostComponent } from './components/post/post.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
 import { PostErrorComponent } from './components/post-error/post-error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostDetailsComponent,
     PostComponent,
+    PostDetailsComponent,
     PostErrorComponent
   ],
   imports: [
@@ -24,6 +25,7 @@ import { PostErrorComponent } from './components/post-error/post-error.component
     MatToolbarModule,
     MatIconModule,
     MatGridListModule,
+    MatProgressSpinnerModule,
     HttpClientModule
   ],
   providers: [],
